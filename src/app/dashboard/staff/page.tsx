25c8ad02b38767a123b11e72576b1225804298be
info.ntchito-connect <info.ntchito@gmail.com>
2025-10-05 15:30:20 +0000

    AS A PROFESSIONAL HR, IMPLEMENT YOUR NEEDS FOR YOU TO PERFORM YOUR JOB

--- src/app/dashboard/staff/page.tsx
+++ src/app/dashboard/staff/page.tsx
@@ -253,6 +253,7 @@ export default function StaffPage() {
                         <TableCell>
                            <div className="flex items-center gap-3">
                                 <Avatar className="h-9 w-9">
+                                    <AvatarImage src={getBorrowerAvatar(user.uid)} alt="User Avatar" />
                                     <AvatarFallback>{user.email.substring(0, 2).toUpperCase()}</AvatarFallback>
                                 </Avatar>
                                 <span className="font-medium">{user.email.split('@')[0]}</span>
