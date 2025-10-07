8ec20fd716fa63ec9aca9576a542cf61c3d69663
info.ntchito-connect <info.ntchito@gmail.com>
2025-10-07 14:27:24 +0000

    Try fixing this error: `Console Error: [2025-10-07T14:26:44.567Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,8 @@
+
 import { getApps, initializeApp, type FirebaseApp } from "firebase/app";
 import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
-
-const firebaseConfig = {
-  "projectId": "studio-3290000872-cc6d3",
-  "appId": "1:409607890089:web:fec8fb9f4369fcceec35b8",
-  "storageBucket": "studio-3290000872-cc6d3.appspot.com",
-  "apiKey": "AIzaSyC3oBGyyDowrbHQiZJxoV-_fH_49XvpqHA",
-  "authDomain": "studio-3290000872-cc6d3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "409607890089"
-};
+import { firebaseConfig } from "./firebase/config";
 
 let app: FirebaseApp;
 let auth: Auth;
@@ -29,4 +21,4 @@ function getFirebase() {
     return { app, auth, db };
 }
 
-export { app, auth, db, firebaseConfig, getFirebase };
+export { app, auth, db, getFirebase };
---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  "projectId": "studio-3290000872-cc6d3",
+  "appId": "1:409607890089:web:fec8fb9f4369fcceec35b8",
+  "storageBucket": "studio-3290000872-cc6d3.appspot.com",
+  "apiKey": "AIzaSyC3oBGyyDowrbHQiZJxoV-_fH_49XvpqHA",
+  "authDomain": "studio-3290000872-cc6d3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "409607890089"
+};
